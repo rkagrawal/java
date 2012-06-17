b3172285b251c72ed313a19afb79250ce582a95d
Rajesh Agrawal <rajesh.agrawal@fare-soft.com>
2012-06-17 04:04:41 -0400

    added hello to universe

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
 	//test class to learn github
 	public static void main( String [] args ) {
 		System.out.println("Hello World\n");
+		System.out.println("Hello Universe\n" );
 	}
 }
