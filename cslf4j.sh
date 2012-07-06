222e7b686a4ec227cff0d9c9707c37e0ae843cfc
Rajesh Agrawal <rajesh.agrawal@fare-soft.com>
2012-07-06 17:11:03 -0400

    compile slf4jtry.java

---
+++ cslf4j.sh
@@ -0,0 +1,2 @@
+javac -classpath ./jars/slf4j-1.6.6/slf4j-api-1.6.6.jar:./jars/slf4j-1.6.6/slf4j-log4j12-1.6.6.jar slf4jtry.java
+java -classpath .:./log4j.properties:./jars/slf4j-1.6.6/slf4j-api-1.6.6.jar:./jars/slf4j-1.6.6/slf4j-log4j12-1.6.6.jar:./jars/slf4j-1.6.6/log4j-1.2.16.jar slf4jtry
